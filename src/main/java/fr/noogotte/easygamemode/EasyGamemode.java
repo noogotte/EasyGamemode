41b16f3f159f2320b7018402e8d6b5bf22b6ace7
noogotte <hugotte.76@live.fr>
2012-05-11 17:13:32 +0200

    First commit

---
+++ src/main/java/fr/noogotte/easygamemode/EasyGamemode.java
@@ -0,0 +1,108 @@
+package main.java.fr.noogotte.easygamemode;
+
+import javax.xml.ws.Response;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EasyGamemode extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        PluginManager pm = getServer().getPluginManager();
+        SignListener signListener = new SignListener(this);
+        pm.registerEvents(signListener, this);
+        getCommand("easy-gamemode").
+            setPermissionMessage(getPermissionMessage());
+    }
+
+    public String getPermissionMessage() {
+        return ChatColor.RED + "Vous n'avez pas la permission !";
+    }
+
+    public void toggleGameMode(Player player) {
+        if (player.getGameMode() == GameMode.CREATIVE) {
+            player.setGameMode(GameMode.SURVIVAL);
+            player.sendMessage(ChatColor.AQUA + "Votre gamemode est maintenant en " +
+                    ChatColor.GOLD + "SURVIVAL");
+        } else { 
+            player.setGameMode(GameMode.CREATIVE);
+            player.sendMessage(ChatColor.AQUA + "Votre gamemode est maintenant en " +
+                    ChatColor.GOLD + "CREATIVE");
+        }
+    }
+
+    public void toggleGameMode(Player responsible, Player target) {
+        if (target.getGameMode() == GameMode.CREATIVE) {
+            target.setGameMode(GameMode.SURVIVAL);
+            target.sendMessage(ChatColor.AQUA + "Le gamemode de " + 
+            		ChatColor.DARK_AQUA + target.getName()+ 
+                    ChatColor.AQUA + " est maintenant en " +
+                    ChatColor.GOLD + "SURVIVAL");
+            responsible.sendMessage(ChatColor.GOLD + "Vous avez changer le gamemode de "
+                    + ChatColor.BLUE + target.getName());
+        } else {
+            target.setGameMode(GameMode.CREATIVE);
+            target.sendMessage(ChatColor.AQUA + "Le gamemode de " +
+                    ChatColor.DARK_AQUA + target.getName() +
+                    ChatColor.AQUA + " est maintenant en " +
+                    ChatColor.GOLD + "CREATIVE");
+            responsible.sendMessage(ChatColor.GOLD + "Vous avez changé le gamemode de " +
+            		ChatColor.DARK_AQUA + target.getName());
+        }
+    }
+    
+    public void fly(Player player) {
+    	player.setFlying(true);
+    	player.sendMessage(ChatColor.BLUE + "Vous etes en mode vol");
+    	
+    }
+    
+    public void fly(Player responsible, Player target) {
+    	target.setFlying(true);
+    	responsible.sendMessage(ChatColor.BLUE + "Vous avez mis" + ChatColor.BOLD+ target.getName() + ChatColor.BLUE +"en mode fly");
+    	
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String lbl, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Commande utilisable seulement en tant que joueur");
+            return true;
+        }
+
+        if (args.length > 2) {
+            return false;
+        }
+
+        Player player = (Player) sender;
+        if (args.length == 0) {
+            if(player.hasPermission("easyG.cmd.me")) {
+                toggleGameMode(player);
+            } else {
+                player.sendMessage(getPermissionMessage());
+            }
+        } else if (args.length == 1) {
+            Player target = Bukkit.getPlayer(args[0]);
+            
+            if(player.hasPermission("easyG.cmd.other")) {
+                if (target == null) {
+                    player.sendMessage(ChatColor.RED + "Le joueur " +
+                            ChatColor.DARK_AQUA + args[0] +
+                            ChatColor.RED + " n'existe pas !");
+                } else {
+                    toggleGameMode(player, target);
+                }
+            } else {
+                player.sendMessage(getPermissionMessage());
+            }
+        } 
+        return true;
+    }
+}
---
+++ src/main/java/fr/noogotte/easygamemode/SignListener.java
@@ -0,0 +1,66 @@
+package main.java.fr.noogotte.easygamemode;
+
+import org.bukkit.ChatColor;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class SignListener implements Listener {
+    public EasyGamemode plugin;
+
+    public SignListener(EasyGamemode easygamemode) {
+        this.plugin = easygamemode;
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onSignChange(SignChangeEvent event) {
+        Player player = event.getPlayer();
+        
+        if (!event.getLine(0).toLowerCase().contains("easyg")) {
+            return;
+        }
+
+        if (!player.hasPermission("easyG.SignPlace")) {
+            event.getBlock().breakNaturally();
+            player.sendMessage(plugin.getPermissionMessage());
+            return;
+        }
+
+        event.setLine(0, ChatColor.RED + "[EasyG]");
+        event.setLine(1, ChatColor.GREEN + "Changement de");
+        event.setLine(2, ChatColor.GREEN + "Gamemode");
+        player.sendMessage(ChatColor.BLUE + "Vous avez créé un panneau de changement de gamemode !");
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onPlayerInteract(PlayerInteractEvent event) {
+    	
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) {
+            return;
+        }
+
+        if (!(event.getClickedBlock().getState() instanceof Sign)) {
+            return;
+        }
+
+        Sign sign = (Sign) event.getClickedBlock().getState();
+        String l1 = sign.getLine(0);
+        if (!l1.contains("[EasyG]")) {
+            return;
+        }
+
+        Player player = event.getPlayer();
+
+        if (!player.hasPermission("easyG.SignUse")) {
+            player.sendMessage(plugin.getPermissionMessage());
+            return;
+        }
+
+        plugin.toggleGameMode(player);
+    }
+}
